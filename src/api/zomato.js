a0066777fcea1ea44bee531afcdedbed890408bc
Anoop <61411852+AS1219@users.noreply.github.com>
2020-09-12 15:53:22 +0530

    Update zomato.js

--- src/api/zomato.js
+++ src/api/zomato.js
@@ -8,12 +8,3 @@ export default axios.create({
 });
 
 
-/*import axios from 'axios';
-
-export default axios.create({
-    baseURL: 'https://api.yelp.com/v3/businesses',
-    headers: {
-        Authorization: 
-            'Bearer qJDdAU7yJJ9MgnjOFLTY9j-dt-nt0udUZ7y1noX0XTxZyg79CBLwLixU79N_oiVqHCPqhI2H1r6xFlhRndSRK9lCtOUCmUwsSS8W6_ldLh4VSZXBLp_v5dWjjF_jXnYx'
-    }
-});*/
