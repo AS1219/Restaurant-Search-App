8e668b29e1840edfdd7ced50f65073eb27b1e621
Anoop <61411852+AS1219@users.noreply.github.com>
2020-09-12 15:44:46 +0530

    Update NearbyScreen.js

--- screens/NearbyScreen.js
+++ screens/NearbyScreen.js
@@ -50,39 +50,3 @@ const styles = StyleSheet.create({
 
 export default NearbyScreen;
 
-/*import React, { useState } from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
-import SearchBar from '../components/SearchBar';
-import useResults from '../src/api/hooks/useResults';
-import ResultsList from '../components/ResultsList';
-
-const NearbyScreen = () => {
-    const [term, setTerm] = useState('');
-    const [searchApi, results, errorMessage] = useResults();
-    
-    const filterResultsByPrice = (price) => {
-        return results.filter(result => {
-            return result.price === price;
-        });
-    };
-
-    return (
-        <> 
-            <SearchBar 
-                term={term} 
-                onTermChange={setTerm} 
-                onTermSubmit={() => searchApi(term)}
-            />
-            {errorMessage ? <Text>{errorMessage}</Text> : null}
-            <ScrollView>
-                <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
-                <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
-                <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
-            </ScrollView>
-        </>
-    );
-};
-
-const styles = StyleSheet.create({});
-
-export default NearbyScreen;*/
