906737dc039f33e49e67d020bc551bd44c4bb449
Anoop <61411852+AS1219@users.noreply.github.com>
2020-09-12 15:45:31 +0530

    Update ResultsShowScreen.js

--- screens/ResultsShowScreen.js
+++ screens/ResultsShowScreen.js
@@ -70,18 +70,3 @@ const styles = StyleSheet.create({
 
 export default ResultsShowScreen;
 
-
-/*import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
-const ResultsShowScreen = () => {
-    return (
-        <View>
-            <Text>Results Show</Text>
-        </View>
-    );
-};
-
-const styles = StyleSheet.create({});
-
-export default ResultsShowScreen;*/
